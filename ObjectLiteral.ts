db9f0feaa32bedd93fb567016d46175f85b682ed
chavantxt <tejaswini.chavan2727@gmail.com>
2024-06-30 12:48:22 +0530

    Object Literals

--- ObjectLiteral.ts
+++ ObjectLiteral.ts
@@ -18,3 +18,10 @@ let p=createPerson("Ross","Geller");
 console.log(p.firstName);
 console.log(p.lastName);
 console.log(p.fullname); 
+
+var employee2:String[] = ["Chanlar","Bing"];
+let fname2:String;
+let lname2:String;
+[fname2,lname2] =  employee2;
+console.log(fname2);
+console.log(lname2);
